e08f449f42456fbb2dc38153eb23f0a8c305a412
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-07-10 18:56:09 +0200

    Add sort of functioning code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args = sylt::Args {
+        file: Some(Path::new("game.sy").to_path_buf()),
+        is_binary: false,
+        compile_target: None,
+        verbosity: 0,
+        help: false,
+        dump_tree: false,
+        skip_typecheck: false,
+    };
+
+    if let Err(errs) = sylt::run_file(&args, sylt::lib_bindings()) {
+        for e in errs.iter().take(5) {
+            eprintln!("{}", e);
+        }
+        if errs.len() > 5 {
+            eprintln!(">>> The other errors were ommited to save you scroll time")
+        }
+    }
 }
