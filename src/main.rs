6c7a863c416cfff0c8c7b62f78e5c2568174775e
Gustav Sörnäs <gustav@sornas.net>
2021-07-11 12:26:05 +0200

    pass other .sy as game file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
     let mut args = sylt::Args::parse_args_default_or_exit();
-    args.file = Some(Path::new("game.sy").to_path_buf());
+    args.file = Some(PathBuf::from(
+        std::env::args()
+        .nth(1)
+        .as_ref()
+        .map(|arg| arg.as_str())
+        .unwrap_or("game.sy")
+    ));
+    println!("{:?}", args);
 
     if let Err(errs) = sylt::run_file(&args, sylt::lib_bindings()) {
         for e in errs.iter().take(5) {
