9ec708d11328319a69cea4eb032a9516b317e2ed
Gustav Sörnäs <gustav@sornas.net>
2021-07-12 18:57:56 +0200

    remove custom arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-use std::path::PathBuf;
-
 fn main() {
-    let mut args = sylt::Args::parse_args_default_or_exit();
-    args.file = Some(PathBuf::from(
-        std::env::args()
-        .nth(1)
-        .as_ref()
-        .map(|arg| arg.as_str())
-        .unwrap_or("game.sy")
-    ));
-    println!("{:?}", args);
+    let args = sylt::Args::parse_args_default_or_exit();
 
     if let Err(errs) = sylt::run_file(&args, sylt::lib_bindings()) {
         for e in errs.iter().take(5) {
